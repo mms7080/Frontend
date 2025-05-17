6aa89371248d2596e17ed16675866902e73b08ad
ParkChaeHun <pch3482@naver.com>
2025-05-18 00:09:08 +0900

    전체 영화 페이지 제작중

--- app/movie/page.jsx
+++ app/movie/page.jsx
@@ -11,30 +11,26 @@ import {fetch} from '../../lib/client';
 export default function Moviepage(){
     
     useEffect(() => {
-        document.title = '전체 영화';
+        document.title = '전체 영화 - 필모라라';
     }, []);
 
-    let headerColor='black';
-    let headerBg='#F9F9F9';
-    let footerColor='black';
-    let footerBg='#F9F9F9';
-    let footerBorder='#ccc';
-
-
     return <>
-        <Header headerColor={headerColor} headerBg={headerBg}></Header>
-        <Box w='calc(100vw - 17px)' minW='1000px'>
+        <Header headerColor="white" headerBg="#1a1a1a"/>
+        <Box bg="white" pt={20} pb={10} px={6} maxW="1280px" mx="auto">
             <Text fontSize="xl" fontWeight="bold" mb={6} borderLeft="4px solid #5f0080" pl={2}>
                 전체 영화
             </Text>
-            <Grid templateColumns='repeat(4, 1fr)' gap='6' w='80%' alignItems='center'>
-                <GridItem><Movie src='https://res.cloudinary.com/upwork-cloud/image/upload/c_scale,w_1000/v1700795880/catalog/1600659718750367744/xiry6ufbjttckqxpfzrw.jpg' name='미션 임파서블' reserve_rate='55.5%' release_date='2025.05.17' like_number='1k'></Movie></GridItem>
-                <GridItem><Movie src='https://res.cloudinary.com/upwork-cloud/image/upload/c_scale,w_1000/v1700795880/catalog/1600659718750367744/xiry6ufbjttckqxpfzrw.jpg' name='미션 임파서블' reserve_rate='55.5%' release_date='2025.05.17' like_number='1k'></Movie></GridItem>
-                <GridItem><Movie src='https://res.cloudinary.com/upwork-cloud/image/upload/c_scale,w_1000/v1700795880/catalog/1600659718750367744/xiry6ufbjttckqxpfzrw.jpg' name='미션 임파서블' reserve_rate='55.5%' release_date='2025.05.17' like_number='1k'></Movie></GridItem>
-                <GridItem><Movie src='https://res.cloudinary.com/upwork-cloud/image/upload/c_scale,w_1000/v1700795880/catalog/1600659718750367744/xiry6ufbjttckqxpfzrw.jpg' name='미션 임파서블' reserve_rate='55.5%' release_date='2025.05.17' like_number='1k'></Movie></GridItem>
-
+            <Grid templateColumns='repeat(4, 1fr)' gap='40px' w='100%' alignItems='center'>
+                    <Movie name='Fields of Destiny' rate='' releaseDate='0000.00.00' likeNumber='5.3k' src='https://res.cloudinary.com/upwork-cloud/image/upload/c_scale,w_1000/v1700795880/catalog/1600659718750367744/xiry6ufbjttckqxpfzrw.jpg'></Movie>
+                    <Movie name='Killer Advice' rate='' releaseDate='0000.00.00' likeNumber='2.1k' src='https://res.cloudinary.com/upwork-cloud/image/upload/c_scale,w_1000/v1700796426/catalog/1600659718750367744/iqmiudmmo6s7zcofwmpf.jpg'></Movie>
+                    <Movie name='InterStella' rate='12' releaseDate='2014.11.06' likeNumber='1.5k' src='https://rukminim2.flixcart.com/image/850/1000/l2dmky80/poster/y/f/b/small-poster-interstellar-sl407-wall-poster-13x19-inches-matte-original-imagdqezkfchjkhz.jpeg?q=20&crop=false'></Movie>
+                    <Movie name='My Name is Alfred Hitchcock' rate='' releaseDate='0000.00.00' likeNumber='1.3k' src='https://www.mvtimes.com/mvt/uploads/2024/11/film-my-name-alfred-hitchcock-2.jpg'></Movie>
+                    <Movie name='어벤져스 엔드게임' rate='12' releaseDate='2019.04.24' likeNumber='986' src='https://upload.wikimedia.org/wikipedia/ko/thumb/f/f2/%EC%96%B4%EB%B2%A4%EC%A0%B8%EC%8A%A4-_%EC%97%94%EB%93%9C%EA%B2%8C%EC%9E%84_%ED%8F%AC%EC%8A%A4%ED%84%B0.jpg/1200px-%EC%96%B4%EB%B2%A4%EC%A0%B8%EC%8A%A4-_%EC%97%94%EB%93%9C%EA%B2%8C%EC%9E%84_%ED%8F%AC%EC%8A%A4%ED%84%B0.jpg'></Movie>
+                    <Movie name='범죄도시 4' rate='15' releaseDate='2024.04.24' likeNumber='734' src='https://i.namu.wiki/i/KwJ2dfIySu2k8JWlK3nD-gS7A9G-2I2EWKkNjoVRqaHabjK88STUo8FXi545XV6Pe8ERSX5DjF4e5k0IkOvznQ.webp'></Movie>
+                    <Movie name='귀멸의칼날 무한성편' releaseDate='2025.08.22' rate='19' likeNumber='521' src='https://i.namu.wiki/i/YvPBZ1kzk8Dku4HhOC2FGB7xKVXj5bpg8cSdRWsAZg-3Knqu5LcWJchrZDIVmz-08V3OV9uFLMfCRNCZRcnTxQ.webp'></Movie>
+                    <Movie name='승부' rate='12' releaseDate='2025.03.26' likeNumber='342' src='https://img.megabox.co.kr/SharedImg/2025/03/27/O6RnDMOAnUw6geDdlaAXRlkqgy0mSSDb_420.jpg'></Movie>
             </Grid>
         </Box>
-        <Footer footerColor={footerColor} footerBg={footerBg} footerBorder={footerBorder}></Footer>
+        <Footer footerColor="white" footerBg="#1a1a1a" footerBorder="transparent" />
         </>;
 }
--- components/movie/movie.jsx
+++ components/movie/movie.jsx
@@ -1,19 +1,85 @@
-import {Flex, Image, Button, Icon, Grid, GridItem} from '@chakra-ui/react';
+"use client"
+import React, {useState} from 'react';
+import { Flex, Image, Button, Text, Box, Kbd, createIcon } from '@chakra-ui/react';
 
-export default function Movie({src, name, reserve_rate, release_date, like_number}) {
-    return <Flex w='100%' h='100%' flexDirection='column'>
-                <Image objectFit='cover' w='100%' h='100%' borderRadius='6px' src={src} loading='lazy'/>
-                <Flex flexDirection='row' gap='6'>
-                    <Image objectFit='cover' w='10%' h='10%' borderRadius='6px' src='https://i.namu.wiki/i/4rqHeSQ7TkE85vF3Vlnz59QUtkq5cE095mHuoGagn8GC1uAic4hrujblFA6fJU1zbqNKuu_5AVw01CRnVQsXMQ.svg' loading='lazy'></Image>
-                    <span>{name}</span>
-                </Flex>
-                <Flex flexDirection='row' justify='center' gap='6'> 
-                    <span>예매율 {reserve_rate}</span>
-                    <span>개봉일 {release_date}</span>
-                </Flex>
-                <Grid templateColumns='repeat(3, 1fr)' gap='6'>
-                    <GridItem><Button>❤️{like_number}</Button></GridItem>
-                    <GridItem><Button paddingLeft='10' paddingRight='10'>예매</Button></GridItem>
-                </Grid>
-            </Flex>;
+const HeartIcon = createIcon({
+  displayName: "HeartIcon",
+  path: (
+    <>
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+      <path
+        stroke="#00aaff"
+        strokeWidth="1.5"
+        fill="currentColor"
+        d="M19.5 13.572l-7.5 7.428l-7.5 -7.428m0 0a5 5 0 1 1 7.5 -6.566a5 5 0 1 1 7.5 6.572"
+      />
+    </>
+  ),
+  viewBox: "0 0 28 28"
+});
+
+export default function Movie({ likeNumber, src, name, rate, releaseDate }) {
+  const [liked, likedController] = useState(false);
+  const likeChange = () => {likedController(!liked)};
+
+  let rateColor = '';
+  if(rate == 'ALL') rateColor = 'green';
+  else if(rate == '12') rateColor = 'yellow';
+  else if(rate == '15') rateColor = 'orange';
+  else if(rate == '19') rateColor = 'red';
+  else {rate='X'; rateColor = 'gray';}
+
+  return (
+    <Flex 
+      w='100%' 
+      h='100%' 
+      flexDirection='column' 
+      bg='white' 
+      border="1px solid #eee" 
+      borderRadius='10px'
+      transition="0.2s"
+      _hover={{ boxShadow: 'xl', transform: 'translateY(-5px)' }}
+    >
+      <Image 
+        objectFit='cover' 
+        w='100%' 
+        h='400px' 
+        borderRadius='6px' 
+        src={src} 
+        loading='lazy'
+      />
+      <Box w="100%" p="3">
+        <Text 
+          noOfLines="1" 
+          fontSize='xl'
+          overflow="hidden"
+          textOverflow="ellipsis"
+          width="100%"
+          css={{
+            WebkitBoxOrient: "vertical",
+            display: "-webkit-box",
+            WebkitLineClamp: 1,
+            overflow: "hidden",
+            textOverflow: "ellipsis",
+            whiteSpace: "normal"
+          }}
+        >
+          <Kbd variant='subtle' colorPalette={rateColor}>{rate}</Kbd> {name}
+        </Text>
+      </Box>
+      <Text px='3' pb='3' fontSize='md'>개봉일 {releaseDate}</Text>
+      <Flex  
+        alignItems='center'
+        px='3'
+        pb='3'
+        gap='3'
+      >
+        <Button bg='white' color='black' border="1px solid #eee"  w='30%' 
+        _hover={{bg:'#ebebeb'}}
+        onClick={likeChange}>
+          <HeartIcon size="lg" color={liked ? "#00aaff" : "white"} /> {likeNumber}</Button>
+        <Button bg='#00aaff' w='65%' _hover={{bg:'#008acf'}}>예매</Button>
+      </Flex>
+    </Flex>
+  );
 }
