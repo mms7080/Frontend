a8dede0768303c19df6a7226a9b30bc852a96788
kkj7584 <kkj758440@gmail.com>
2025-05-18 22:22:36 +0900

    네비게이터 홈 로고 누르며 홈으로 이동하게 수정

--- components/navigator.jsx
+++ components/navigator.jsx
@@ -1,6 +1,7 @@
 import {Flex} from "@chakra-ui/react";
 import {Tooltip} from "./ui/tooltip"
 import {FaCaretUp,FaCaretDown,FaHome} from 'react-icons/fa';
+import Link from 'next/link';
 
 export default function FixedRightSideNavigator() {
   /* 스크롤 이벤트 핸들러 */
@@ -28,10 +29,12 @@ export default function FixedRightSideNavigator() {
             <FaCaretUp size='50px' color='gray'/>
         </Flex>
       </Tooltip>
-      <Tooltip content="즐겨찾기" positioning={{placement:"left"}} showArrow openDelay={100} closeDelay={100}>
-        <Flex w='50px' h='50px' borderRadius='5px' justifyContent='center' alignItems='center' onClick={()=>alert("Ctrl+D를 눌러 이 페이지를 즐겨찾기에 추가하세요")} transition='all 0.25s ease-in 0s' _hover={{bg:'#E5E5E5',cursor:'pointer'}}>{/* 아래로 스크롤 작동 */}
-            <FaHome size='30px' color='gray'/>
-        </Flex>
+      <Tooltip content="홈" positioning={{placement:"left"}} showArrow openDelay={100} closeDelay={100}>
+        <Link href='/home'>
+          <Flex w='50px' h='50px' borderRadius='5px' justifyContent='center' alignItems='center' transition='all 0.25s ease-in 0s' _hover={{bg:'#E5E5E5',cursor:'pointer'}}>{/* 아래로 스크롤 작동 */}
+              <FaHome size='30px' color='gray'/>
+          </Flex>
+        </Link>
       </Tooltip>
       <Tooltip content="아래로" positioning={{placement:"left"}} showArrow openDelay={100} closeDelay={100}>
         <Flex w='50px' h='50px' borderRadius='5px' justifyContent='center' alignItems='center' onClick={()=>handleScroll("down")} transition='all 0.25s ease-in 0s' _hover={{bg:'#E5E5E5',cursor:'pointer'}}>{/* 아래로 스크롤 작동 */}
