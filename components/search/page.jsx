a650ae058243ac5fa2316fa23a9ccc4ae2182d01
kkj7584 <kkj758440@gmail.com>
2025-05-29 21:47:32 +0900

    검색 페이지에서 검색하면 영화, 이벤트 모두 검색하도록 변경

--- components/search/page.jsx
+++ components/search/page.jsx
@@ -1,31 +1,33 @@
 'use client';
 
-import React,{useEffect,useState} from 'react';
+import React,{useEffect,useState,useMemo} from 'react';
 import {Button,Flex,Box,Input,Text} from '@chakra-ui/react';
+import {useRouter} from 'next/navigation';
 import Link from "next/link";
 
 import {Header} from '..';
 import MovieCard from '../movie/moviecard';
+import Event from '../element/event';
 
 export default function Searchdetail({userData,movieData,serverEvents,keywordData}){
+    const router = useRouter();
     const [movies, setMovies] = useState(movieData);
-    const [inputValue, setInputValue] = useState(keywordData);
     const [searchWord, setSearchWord] = useState(keywordData);
-    const [displayNumber, setDisplayNumber] = useState(8);
+    const [displayNumber, setDisplayNumber] = useState(8);/* 검색된 영화 더보기 버튼 */
+    const [displayNumber2, setDisplayNumber2] = useState(8);/* 검색된 이벤트 더보기 버튼 */
     const [events] = useState(serverEvents || {});
 
     const handleSearch = () => {
-        if(inputValue != searchWord) 
-            setSearchWord(inputValue);
+        router.push(`/search/${document.querySelector('#keyword').value}`);
     }    
     
     // 검색어 포함 유무로 분류
     const searchedMovies = searchWord === "" ? movies:
     movies.filter((movie) => {
-        return movie.title.includes(searchWord);
+        return movie.title.toLowerCase().includes(searchWord.toLowerCase());
     });
     
-    // 더보기 버튼
+    // 영화 더보기 버튼
     const MoreButton = () => {
         if(displayNumber < searchedMovies.length)
             return (<Box pt={10} >
@@ -39,6 +41,7 @@ export default function Searchdetail({userData,movieData,serverEvents,keywordDat
 
     useEffect(()=>{
         setDisplayNumber(8);
+        setDisplayNumber2(8);
     },[searchWord]);
 
     {/* 영화카드들 */}
@@ -59,24 +62,49 @@ export default function Searchdetail({userData,movieData,serverEvents,keywordDat
                     </Box>)
     }
 
-    // const EventCards = () => {
-    //     if(searchWord != "" && searchedMovies.length < 1)
-    //         return <Box w='100%' h='50vh' bg='#1e1e1e' fontSize='4xl' color='white'
-    //                 display='flex' alignItems='center' justifyContent='center'>
-    //                 검색 결과가 없습니다
-    //                 </Box>
-    //     else return (<Box className="movie-grid">
-    //                     {searchedMovies.map((movie,index) => {
-    //                         if(index < displayNumber)
-    //                             return (<MovieCard 
-    //                                         key={movie.id}
-    //                                         movie={movie}
-    //                                     />)
-    //                     })}
-    //                 </Box>)
-    // }
+    const categoryOrder = ["Pick", "영화", "극장", "제휴/할인", "시사회/무대인사"];
+    const arrangedEvents=useMemo(()=>{
+        return categoryOrder.filter((cat) => events[cat]).map((cat) => [cat, events[cat]]);/* 전체 이벤트를 포함하는 변수 생성 */
+    },[events]);
 
-    
+    const searchedEvents = useMemo(()=>{
+        return searchWord === "" ? arrangedEvents:arrangedEvents/* 검색 키워드를 포함하는 이벤트 추려내기 */
+        .map(([category, items]) => [
+          category,
+          items.filter((e) =>
+            e.title.toLowerCase().includes(searchWord.toLowerCase())
+          ),
+        ])
+        .filter(([_, items]) => items.length > 0);
+    },[searchWord, arrangedEvents]); 
+
+    // 이벤트 더보기 버튼
+    const MoreButton2 = () => {
+        if(displayNumber2 < searchedEvents.flat().filter((_,index)=>index%2=== 1).flat().length)
+            return (<Box pt={10} >
+                        <Button
+                            w='100%' bg="#1e1e1e" border="1px solid gray" 
+                            _hover={{borderColor : "white"}}
+                            onClick={()=>{setDisplayNumber2(displayNumber2+8)}}
+                        >더보기</Button>
+                    </Box>)
+    };        
+
+    const EventCards = () => {/* 이벤트 카드들 */
+        if(searchWord != "" && searchedEvents.length < 1)
+            return <Box w='100%' h='50vh' bg='#1e1e1e' fontSize='4xl' color='white'
+                    display='flex' alignItems='center' justifyContent='center'>
+                    검색 결과가 없습니다
+                    </Box>
+        else return (<Box className="movie-grid" overflow='visible'>
+                    {searchedEvents.flat().filter((_,index)=>index%2=== 1).flat().map((items,index) =>{
+                        if(index<displayNumber2)
+                            return <Box overflow='visible' key={items.id}>
+                                    <Event content={items.title} src={`${process.env.NEXT_PUBLIC_SPRING_SERVER_URL}${items.image}`} url={`/event/view/${items.id}`}></Event>
+                                </Box>;
+                    })}
+                </Box>)
+    }    
 
     return <>
         <Header userInfo={userData}></Header>
@@ -86,14 +114,13 @@ export default function Searchdetail({userData,movieData,serverEvents,keywordDat
                     <Flex gap={2} justify={'space-between'} >
                         <Box transform="translate(-23px, 0)">
                             <Input
-                                placeholder="영화명 검색"
+                                id='keyword'
+                                placeholder="키워드 입력"
                                 w="320px" p="10px" bg="#1e1e1e"
                                 border="1px solid gray"
                                 fontSize="15px" color="white"
                                 _hover={{borderColor : "white"}}
-                                value={inputValue}
                                 ml='25px'
-                                onChange={(e) => setInputValue(e.target.value)}
                                 onKeyDown={(e) => {
                                     if(e.key === 'Enter') handleSearch();
                                 }}
@@ -138,11 +165,14 @@ export default function Searchdetail({userData,movieData,serverEvents,keywordDat
                 pl='10px'
                 py='10px'
                 mt='30px'
+                mb='30px'
                 borderLeft="4px solid #6B46C1"
                 color='white'
                 >
                     이벤트 검색결과
                 </Text>
+                <EventCards/>
+                <MoreButton2/>
             </Box>
         </div>
         </>;
