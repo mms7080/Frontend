d5fdd258b3eacb3f007f7aff9721963401dcf7e0
bum <mms7080@naver.com>
2025-05-30 03:56:01 +0900

    이벤트 업로드 메가Pick > Pick

--- components/event/upload/page.jsx
+++ components/event/upload/page.jsx
@@ -188,7 +188,7 @@ export default function EventUploader({userData}) {
             >
               <option value="">카테고리 선택</option>
               <option value="추천">추천</option>
-              <option value="메가Pick">메가Pick</option>
+              <option value="Pick">Pick</option>
               <option value="영화">영화</option>
               <option value="극장">극장</option>
               <option value="제휴/할인">제휴/할인</option>
