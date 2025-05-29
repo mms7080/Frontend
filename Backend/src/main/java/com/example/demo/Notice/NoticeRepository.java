c5b4cb99010f7d13e0be7b48a7d92ae67dc24abb
hongsipapa <wnsghl9897@naver.com>
2025-05-29 09:44:29 +0900

    Delete Backend directory

--- Backend/src/main/java/com/example/demo/Notice/NoticeRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.demo.Notice;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NoticeRepository extends JpaRepository<Notice, Long> {
-}
--- Backend/src/main/java/com/example/demo/Payment/PaymentRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.demo.Payment;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PaymentRepository extends JpaRepository<Payment, Long> {
-}
