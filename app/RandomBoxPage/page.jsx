e7dc2a406a39226d4b19b5a060cb9d7130d6607a
bum <mms7080@naver.com>
2025-05-31 11:01:46 +0900

    오류수정

--- app/RandomBoxPage/page.jsx
+++ app/RandomBoxPage/page.jsx
@@ -2,7 +2,7 @@ import RandomBoxPage from "../../components/RandomBox/page";
 import { cookies } from "next/headers";
 
 export default async function Page() {
-  const cookieStore = cookies();
+  const cookieStore = await cookies();
   const res = await fetch(`${process.env.NEXT_PUBLIC_SPRING_SERVER_URL}/userinfo`, {
     headers: {
       Cookie: cookieStore.toString(),
