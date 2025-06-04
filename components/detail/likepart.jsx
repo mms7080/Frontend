c1acfdf3ef00170bebe1b5790aa622b5f8b35582
kkj7584 <kkj758440@gmail.com>
2025-06-05 05:59:08 +0900

    공유하기 버튼 기능 추가

--- components/detail/likepart.jsx
+++ components/detail/likepart.jsx
@@ -9,6 +9,17 @@ import {AiOutlineHeart,AiFillHeart} from 'react-icons/ai';
 
 export default function LikePart({id,res,movieinfo}){
 
+    const address = `http://localhost:3000/detail/${id}`; // 복사할 주소
+
+    const handleCopy = async () => {
+      try {
+        await navigator.clipboard.writeText(address);
+        alert('현재 페이지의 주소가 클립보드에 복사되었습니다!');
+      } catch (err) {
+        alert('클립보드 접근이 차단되었습니다.');
+      }
+    };
+
     const [liked, likedController] = useState(res?(res.likemovies.includes(Number(id))):false);
     const [likeNumber, setLikeNumber] = useState(movieinfo.likeNumber > 999 ? Math.floor(movieinfo.likeNumber / 100) / 10 + 'k' : movieinfo.likeNumber);
     const {isModalOpen, isModalVisible, openModal, closeModal} = useModal();
@@ -30,7 +41,7 @@ export default function LikePart({id,res,movieinfo}){
         <Button fontSize='15px' boxShadow='4px 4px 6px black' onClick={likeChange}>{liked?<AiFillHeart style={{width:25,height:25}} color='red'/>:<AiOutlineHeart style={{width:25,height:25}} color='red'/>}{
             likeNumber                             
         }</Button>
-        <Button fontSize='15px' boxShadow='4px 4px 6px black'>공유하기</Button>
+        <Button fontSize='15px' boxShadow='4px 4px 6px black' onClick={handleCopy}>공유하기</Button>
         {isModalOpen && (<Modal
         isModalOpen={isModalOpen}
         isModalVisible={isModalVisible}
