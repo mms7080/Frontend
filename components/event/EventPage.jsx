da04993e2cfc6013cbcc529ffa834a6d7425f54b
kkj7584 <kkj758440@gmail.com>
2025-06-06 01:35:01 +0900

    이벤트에서도 대소문자, 공백 무시하고 검색하도록 수정

--- components/event/EventPage.jsx
+++ components/event/EventPage.jsx
@@ -59,7 +59,7 @@ export default function EventPage({ serverEvents, userData }) {
     .map(([category, items]) => [
       category,
       items.filter((e) =>
-        e.title.toLowerCase().includes(confirmedKeyword.toLowerCase())
+        e.title.replace(/\s+/g, '').toLowerCase().includes(confirmedKeyword.replace(/\s+/g, '').toLowerCase())
       ),
     ])
     .filter(([_, items]) => items.length > 0);
