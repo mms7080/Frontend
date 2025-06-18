c88802cf614d995feb8ded9c0a2768c00a2cd567
kkj7584 <kkj758440@gmail.com>
2025-06-18 19:39:23 +0900

    탭 제목 통일중

--- app/movie/upload/page.jsx
+++ app/movie/upload/page.jsx
@@ -2,6 +2,11 @@ import {Header} from '../../../components';
 import {fetch} from "../../../lib/server";
 import MovieUploader from "../../../components/movie/upload/movieuploader";
 
+export const metadata = {
+    title: '영화 업로드 - FILMORA',
+    description: '필모라는 최신 영화를 예매하고 리뷰할 수 있는 사이트입니다.',
+};
+
 export default async function MovieUploadPage() {
     const userRes = await fetch(`${process.env.NEXT_PUBLIC_SPRING_SERVER_URL}/userinfo`);
 
